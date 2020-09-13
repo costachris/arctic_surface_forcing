94f3d39f987252ef2087c52a6332b2f304d2e74b
costachris <christopouloscosta@gmail.com>
2020-09-13 12:20:01 -0700

    updated script for fetching profiles from merra reanalyis and included automated julia script for computing cs matrices from generated profiles

--- data_download/download_merra2.py
+++ data_download/download_merra2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[69]:
+# In[2]:
 
 
 import xarray as xr
@@ -13,17 +13,18 @@ import requests
 import pandas as pd
 
 
-# In[ ]:
+# In[18]:
 
 
-dates = pd.date_range('2015-01-01', '2015-01-07', freq = 'D')
+dates = pd.date_range('2010-01-01', '2016-06-01', freq = 'D')
 
 
-# In[98]:
+# In[19]:
 
 
 # output_data_dir = '/export/data1/cchristo/merra2/merra2_pres_levels/'
-output_data_dir = '/home/cchristo/proj_christian/data/reanalysis_3d/merra2/'
+# output_data_dir = '/home/cchristo/proj_christian/data/reanalysis_3d/merra2/'
+output_data_dir = '/export/data2/groupMembers/cchristo/reanalysis_3d/merra2/'
 
 # string formats for saving locally
 output_rel_subpath_format = '{year}/{month:02d}/'
@@ -34,7 +35,7 @@ output_rel_path_format = output_data_dir + output_rel_subpath_format
 input_url_format = 'https://goldsmr5.gesdisc.eosdis.nasa.gov/data/MERRA2/M2T3NVASM.5.12.4/{year}/{month:02d}/MERRA2_400.tavg3_3d_asm_Nv.{year}{month:02d}{day:02d}.nc4'
 
 
-# In[102]:
+# In[20]:
 
 
 for date_i in dates:
@@ -43,10 +44,19 @@ for date_i in dates:
     output_rel_path = output_rel_path_format.format(year = date_i.year, month = date_i.month, day = date_i.day)
     output_file_path = output_file_path_format.format(year = date_i.year, month = date_i.month, day = date_i.day)
     try:
+        # make rel dir if it doesn't already exist
+#         if not os.path.exists(output_rel_path):
+#             os.makedirs(output_rel_path)
 #       if file aleady exists move on
         if not os.path.exists(output_file_path):
             wget_string = 'wget --load-cookies ~/.urs_cookies --save-cookies ~/.urs_cookies --auth-no-challenge=on --keep-session-cookies --content-disposition ' + URL + ' --directory-prefix ' + output_rel_path       
             os.system(wget_string)
-    except:
-        print('Could not download ', date_i)
+    except Exception as e:
+        print('Could not download ', date_i, ' : ', e)
+
+
+# In[9]:
+
+
+output_rel_path
 
---
+++ rad_transfer_python/.ipynb_checkpoints/generate_cs_matrix_julia_auto-checkpoint.jl
@@ -0,0 +1,88 @@
+using Pkg
+Pkg.activate("rad_transfer"; shared=true)
+using RadiativeTransfer
+using RadiativeTransfer.CrossSection
+using KernelAbstractions
+using ..Architectures
+using Pkg.Artifacts
+using ProgressMeter
+using DataStructures
+using PyPlot
+using CUDA
+using Flux
+using NetCDF
+using NCDatasets
+using Dates
+
+include("julia_cs_matrix_helper.jl")
+
+profile_ts_file_path = "/net/fluo/data2/groupMembers/cchristo/profiles/summit_merra/summit_all.nc"
+rel_path_name = "/net/fluo/data2/groupMembers/cchristo/cs_matrices/summit/julia_generated/"
+
+# open hitran tables
+hitran_data_CO2 = read_hitran("hitran_molec_id_2_CO2.tar", mol=2, iso=1, ν_min=400, ν_max=2100)
+hitran_data_H2O = read_hitran("hitran_molec_id_1_H2O.tar", mol=1, iso=1, ν_min=400, ν_max=2100)
+hitran_data_CH4 = read_hitran("hitran_molec_id_6_CH4.tar", mol=6, iso=1, ν_min=400, ν_max=2100)
+
+# open profile timeseries
+ds = Dataset(profile_ts_file_path) ;
+
+
+
+
+for time_i in 1260:length(ds["time"])
+    
+    T = ds["T"][:,time_i]
+    P = ds["PL"][:,time_i]
+    time_ii = ds["time"][time_i]
+    
+    display(time_ii)
+    # (lon, lat, z, time)
+    var_array_sizes = size(T)
+    
+    # define grids 
+    ν_grid = 400:0.01:2099.99
+    p_grid = P[:] / 100
+    t_grid = T[:]
+    
+    # compute cs matrix
+    CO2_cs_matrix_cuda = get_cross_section_matrix(hitran_data_CO2, ν_grid, p_grid, t_grid)
+    H2O_cs_matrix_cuda = get_cross_section_matrix(hitran_data_H2O, ν_grid, p_grid, t_grid)
+    CH4_cs_matrix_cuda = get_cross_section_matrix(hitran_data_CH4, ν_grid, p_grid, t_grid)
+    
+    # convert to regular array
+    CO2_cs_matrix = convert(Array, CO2_cs_matrix_cuda)
+    H2O_cs_matrix = convert(Array, H2O_cs_matrix_cuda)
+    CH4_cs_matrix = convert(Array, CH4_cs_matrix_cuda)
+    
+    
+    fname = string(year(time_ii),"/cs_matrix_",year(time_ii), lpad(month(time_ii),2, "0"), lpad(day(time_ii),2,"0"), 
+    "_", lpad(hour(time_ii),2,"0"), lpad(minute(time_ii),2,"0"), ".nc")
+
+
+    out_file_path = string(rel_path_name, fname)
+
+
+    ds_out = Dataset(out_file_path,"c")
+
+    nu_dim = defDim(ds_out,"nu", size(ν_grid)[1])
+    pres_dim = defDim(ds_out,"pressure", size(p_grid)[1])
+
+
+
+
+    # Define the variables temperature with the attribute units
+    CO2_cs_matrix_var = defVar(ds_out,"cs_matrix_co2", Float32, ("pressure", "nu"))
+    H2O_cs_matrix_var = defVar(ds_out, "cs_matrix_h2o", Float32, ("pressure", "nu"))
+    CH4_cs_matrix_var = defVar(ds_out,"cs_matrix_ch4", Float32, ("pressure", "nu"))
+
+
+    CO2_cs_matrix_var[:,:] = CO2_cs_matrix
+    H2O_cs_matrix_var[:,:] = H2O_cs_matrix
+    CH4_cs_matrix_var[:,:] = CH4_cs_matrix
+
+
+
+    close(ds_out)
+
+end
